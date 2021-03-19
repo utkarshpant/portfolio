3ed987b4920fddec594d1e93554b5ead5fbd5be3
Utkarsh Pant <utkarsh.pant@outlook.com>
2021-03-19 20:52:28 +0530

    Corrected header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,13 +6,13 @@ const Header = (props) => {
     return (
         <Container fluid className="HeaderContainer">
             <Row className="Header">
-                <Col md={2} className="HeaderItem">
+                <Col md={2} sm={1} className="HeaderItem">
                     <a href="#about" className="HeaderLink">About</a>
                 </Col>
-                <Col md={2} className="HeaderItem">
+                <Col md={2} sm={1} className="HeaderItem">
                     <a href="#work" className="HeaderLink">Work</a>
                 </Col>
-                <Col md={2} className="HeaderItem">
+                <Col md={2} sm={1} className="HeaderItem">
                     <a href="https://drive.google.com/file/d/1C2BBSJy9Mb7D3EkVOV8OsjyL8n6nHgiz/view?usp=sharing" className="HeaderLink">Resume</a>
                 </Col>
             </Row>
